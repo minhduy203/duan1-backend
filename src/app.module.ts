f965ec950c7ce0bf8923403aa3b708b19b61436e
duongminhduy <duy.duong@usumsoftware.com>
2021-11-29 09:32:54 +0700

    add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/demo'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://minhduy203:Minhduy203+97@webnhaxe.n4drt.mongodb.net/duan1?retryWrites=true&w=majority'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
